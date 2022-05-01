56012c3bae33bb7bb4085f7555a095ec588e0373
Juan Garcia <juan.garciacostas@cm.com>
2022-05-01 15:30:44 +0200

    Fix the path for the build

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/dutch-verbs-vue" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/dutch-verbs-vue/" : "/dutch-verbs-vue/",
 };
