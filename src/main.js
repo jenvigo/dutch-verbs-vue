c2d555fffe3b797fb21fc6b686fda31af9a16950
Juan Garcia <juan.garciacostas@cm.com>
2022-06-06 23:04:41 +0200

    todos to integrate vue with webpack

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import VerbsInput from './components/VerbsInput.vue';
 const app = createApp(App);
 app.config.devtools = true;
 
+/* todo: add the index.js here so it's our main entrypoint and we can use the methods in the components */
+/* todo: add webpack webserver and the npm scripts of this tutorial: https://www.youtube.com/watch?v=r3JpKINIVH8 */
+
 // app.component('friend-contact', FriendContact);
 // app.component('new-friend', NewFriend);
 // app.component('verbs-input', VerbsInput);
